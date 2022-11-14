61e87ff1152a211aaa5662bc1b16a7dc7285ec7b
Grady253 <grady.andre2@gmail.com>
2022-11-13 18:50:44 -0800

    add mutations within the resolver function.

--- server/schemas/resolver.js
+++ server/schemas/resolver.js
@@ -1,3 +1,34 @@
-const {user} = require('../models');
+const {user, User} = require('../models');
 const {AuthenticationError} = require('apollo-server-express');
 const {signToken} = require('../utils/auth');
+const { findOne } = require('../models/User');
+
+const resolvers = {
+    query:{
+        me: async(parent, args, context) =>{
+            if(context.user){
+                const userData = findOne({_id: context.user._id})
+                return userData
+            }
+            throw new AuthenticationError('Not Logged In');
+        }
+    },
+    mutation:{
+        addUser: async(parent, args) =>{
+            const user = await user.create(args);
+            const token =signToken(user);
+            return {user, token};
+        },
+
+        login: async (parent,{email,password})=> {
+            const user = await User.findOne({ email });
+            if (!user){
+                throw new AuthenticationError('Not Logged In');
+            }
+            const token = signToken(user);
+            return {user, token};
+        }
+
+        
+    }
+}
