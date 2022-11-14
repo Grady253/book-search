f9ad7b4166d43b4cfbb718caad3150f49d493678
Grady253 <grady.andre2@gmail.com>
2022-11-13 17:35:36 -0800

    Got login mutation functionality to work.

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -4,11 +4,13 @@ import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '../utils/mutations';
 import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
+import auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const [login, {error}] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -26,15 +28,19 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      // const response = await loginUser(userFormData);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      // const { token, user } = await response.json();
+      // console.log(user);
+      // Auth.login(token);
+      const {data} = await login({
+        variables:{...userFormData}
+      });
+      auth.login(data.login.token)
     } catch (err) {
       console.error(err);
       setShowAlert(true);
