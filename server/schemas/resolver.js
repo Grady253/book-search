1c8790894b5a8aaec2ccda4a49b437b622a83514
Grady253 <grady.andre2@gmail.com>
2022-11-13 19:34:35 -0800

    added saveBook and removeBook mutations

--- server/schemas/resolver.js
+++ server/schemas/resolver.js
@@ -8,7 +8,7 @@ const resolvers = {
         me: async(parent, args, context) =>{
             if(context.user){
                 const userData = findOne({_id: context.user._id})
-                return userData
+                return userData;
             }
             throw new AuthenticationError('Not Logged In');
         }
@@ -20,15 +20,46 @@ const resolvers = {
             return {user, token};
         },
 
-        login: async (parent,{email,password})=> {
-            const user = await User.findOne({ email });
+        login: async (parent, {email, password})=> {
+            const user = await user.findOne({ email });
             if (!user){
                 throw new AuthenticationError('Not Logged In');
             }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if(!correctPw){
+                throw new AuthenticationError('Incorrect password'); 
+            }
+
             const token = signToken(user);
+
             return {user, token};
+        },
+
+        saveBook: async (parent, args, context) =>{
+            if(context.user){
+                const updateUser = await user.findOneAndUpdate(
+                    {_id: context.user._id},
+                    {$push:{saveBook: args}},
+                    {new: true, runValidtors: true}
+                );
+                return updateUser;
+            }
+            throw new AuthenticationError('Not Logged In');
+        },
+
+        removeBook: async (parent, {bookId}, context) =>{
+            if(context.user){
+                const updateUser = await user.findOneAndUpdate(
+                    {_id: context.user._id},
+                    {$pull:{saveBook: {bookId}}},
+                    {new: true}
+                );
+                return updateUser;
+            }
+            throw new AuthenticationError('Not Logged In');
         }
 
-        
     }
 }
