487bd185c521351e9636938162e59ad39675f029
Grady253 <grady.andre2@gmail.com>
2022-11-13 21:59:36 -0800

    fixed the server.js file.

--- server/server.js
+++ server/server.js
@@ -4,17 +4,16 @@ const db = require('./config/connection');
 
 const {ApolloServer}= require('apollo-server-express');
 const {authMiddleware} = require('./utils/auth');
-const {typeDefs, resolvers} = require('./schemas');
+const {typeDefs, resolver} = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
-  resolvers,
+  resolver,
   context: authMiddleware,
 });
 
-server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -23,15 +22,21 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+const StartApolloServer = async (typeDefs, resolver) =>{
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname,'../client/build/index.html'));
+  });
 
-app.get('*', (req, res)=>{
-  res.sendFile(path.join(__dirname,'../client/build/index.html'));
-});
+  db.once('open', () => {
+      app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+      })
+  });
 
+};
 
-db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    })
-});
+StartApolloServer(typeDefs,resolver);
